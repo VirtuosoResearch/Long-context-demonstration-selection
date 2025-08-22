5532fc0486289a97538c3c24f7332884d5122bcf
MichaelZona <2462970640@qq.com>
2025-08-22 18:40:28 -0400

    update

--- scripts/inference_SWE_llama-7b.sh
+++ scripts/inference_SWE_llama-7b.sh
@@ -1,7 +1,15 @@
+# python -m swebench.inference.run_llama \
+#   --dataset_path princeton-nlp/SWE-bench_oracle \
+#   --model_name_or_path princeton-nlp/SWE-Llama-7b \
+#   --output_dir ./outputs/swe-llama-7b \
+#   --split test \
+#   --temperature 0 \
+#   --top_p 1
+
 python -m swebench.inference.run_llama \
-  --dataset_path princeton-nlp/SWE-bench_oracle \
-  --model_name_or_path princeton-nlp/SWE-Llama-7b \
-  --output_dir ./outputs/swe-llama-7b \
+  --dataset_path ./datasets/oracle_lite_test \
+  --model_name_or_path meta-llama/Llama-3.2-1B \
+  --output_dir ./outputs/llama-3.2-1b_lite_test \
   --split test \
   --temperature 0 \
   --top_p 1
