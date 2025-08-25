8298bbf07149cead910f9ec5f2a3d6faf049c7ba
MichaelZona <2462970640@qq.com>
2025-08-25 19:18:07 -0400

    update

--- scripts/inference_SWE_llama-7b.sh
+++ scripts/inference_SWE_llama-7b.sh
@@ -6,18 +6,18 @@
 #   --temperature 0 \
 #   --top_p 1
 
-python -m swebench.inference.run_llama \
-  --dataset_path ./datasets/oracle_lite_test \
-  --model_name_or_path meta-llama/Llama-3.2-1B \
-  --output_dir ./outputs/llama-3.2-1b_lite_test \
-  --split test \
-  --temperature 0 \
-  --top_p 1
+# python -m swebench.inference.run_llama \
+#   --dataset_path ./datasets/oracle_lite_test \
+#   --model_name_or_path meta-llama/Llama-3.2-1B \
+#   --output_dir ./outputs/llama-3.2-1b_lite_test \
+#   --split test \
+#   --temperature 0 \
+#   --top_p 1
 
 python -m swebench.inference.run_llama \
   --dataset_path ./datasets/oracle_lite_test \
-  --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
-  --output_dir ./outputs/llama-3.2-1b-instruct_lite_test \
+  --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
+  --output_dir ./outputs/llama-3.2-3b-instruct_lite_test \
   --split test \
   --temperature 0 \
   --top_p 1
--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -1,5 +1,6 @@
 python -m swebench.harness.run_evaluation \
     --dataset_name princeton-nlp/SWE-bench_Lite \
-    --predictions_path 'gold' \
-    --max_workers 8 \
-    --run_id test_gold_all
+    --split test \
+    --predictions_path /home/michael/project/MTL-SWE-agents/outputs/llama-3.2-3b-instruct_lite_test/oracle_lite_test__test__meta-llama__Llama-3.2-3B-Instruct__temp-0.0__top-p-1.0.jsonl \
+    --max_workers 3 \
+    --run_id test_llama_3b-instruct
