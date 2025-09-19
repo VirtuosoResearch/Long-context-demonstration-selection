b7efc33932bbba8dd70a3391ab90329a6ec6ad46
MichaelZona <2462970640@qq.com>
2025-09-19 14:37:17 -0400

    update

--- scripts/inference_SWE_llama-7b.sh
+++ scripts/inference_SWE_llama-7b.sh
@@ -24,18 +24,26 @@
 
 # codellama/CodeLlama-7b-Instruct-hf
 
-# python -m swebench.inference.run_llama \
-#   --dataset_path ./datasets/oracle_lite_test \
-#   --model_name_or_path princeton-nlp/SWE-Llama-7b \
-#   --output_dir ./outputs/codellama-7b-instruct_lite_test \
-#   --split test \
-#   --temperature 0 \
-#   --top_p 1
-
 python -m swebench.inference.run_llama \
-  --dataset_path ./datasets/oracle_lite_test \
-  --model_name_or_path Qwen/Qwen2.5-Coder-7B-Instruct \
-  --output_dir ./outputs/codellama-7b-instruct_lite_test \
+  --dataset_path /home/michael/project/MTL-SWE-agents/datasets/oracle_lite_test_bugfix \
+  --model_name_or_path /home/michael/project/MTL-SWE-agents/model/SWE-Llama-7bbugfix_lora_merged \
+  --output_dir ./outputs/swellama-7b_lite_test_ft_on_bm25_bugfix \
   --split test \
   --temperature 0 \
   --top_p 1
+
+python -m swebench.inference.run_llama \
+  --dataset_path /home/michael/project/MTL-SWE-agents/datasets/oracle_lite_test_feature \
+  --model_name_or_path /home/michael/project/MTL-SWE-agents/model/SWE-Llama-7bfeature_lora_merged \
+  --output_dir ./outputs/swellama-7b_lite_test_ft_on_bm25_feature \
+  --split test \
+  --temperature 0 \
+  --top_p 1
+
+# python -m swebench.inference.run_llama \
+#   --dataset_path ./datasets/oracle_lite_test \
+#   --model_name_or_path Qwen/Qwen2.5-Coder-7B-Instruct \
+#   --output_dir ./outputs/codellama-7b-instruct_lite_test \
+#   --split test \
+#   --temperature 0 \
+  # --top_p 1
--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -5,8 +5,8 @@
 #     --max_workers 3 \
 #     --run_id test_llama_3b-instruct
 python -m swebench.harness.run_evaluation \
-    --dataset_name princeton-nlp/SWE-bench \
+    --dataset_name princeton-nlp/SWE-bench_Lite \
     --split test \
-    --predictions_path gold \
+    --predictions_path /home/michael/project/MTL-SWE-agents/outputs/swellama-7b_lite_test_ft_on_bm25_bugfix/oracle_lite_test_bugfix__test__SWE-Llama-7bbugfix_lora_merged__temp-0.0__top-p-1.0.jsonl \
     --max_workers 3 \
-    --run_id test_gold_all
+    --run_id test_lite_ftonbm25
