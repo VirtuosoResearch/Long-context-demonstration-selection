c4e66d4e4aae6a4d603f24702f5978a8b0fa8e2f
MichaelZona <2462970640@qq.com>
2025-10-17 15:36:22 -0400

    update

--- src/personalized/self_agent.sh
+++ src/personalized/self_agent.sh
@@ -1,28 +1,3 @@
-
-
-python src/personalized/self_agent_java.py \
-  --model Qwen/Qwen2.5-Coder-7B-Instruct \
-  --engine transformers \
-  --max-new-tokens 256 \
-  --num-iter 10 \
-  --timeout 5
-
-
-python src/personalized/self_agent_java.py \
-  --model codellama/CodeLlama-7b-Instruct-hf \
-  --engine transformers \
-  --max-new-tokens 256 \
-  --num-iter 10 \
-  --timeout 5
-
-
-python src/personalized/self_agent_java.py \
-  --model google/codegemma-7b \
-  --engine transformers \
-  --max-new-tokens 256 \
-  --num-iter 10 \
-  --timeout 5
-
 python src/personalized/self_agent_multi.py --lang js --model Qwen/Qwen2.5-Coder-7B-Instruct
 python src/personalized/self_agent_multi.py --lang js --model codellama/CodeLlama-7b-Instruct-hf
 python src/personalized/self_agent_multi.py --lang js --model google/codegemma-7b
